0b1c023becc9c6ea95955117a5908cf84ef4e72f
guledali <guled.ali01@gmail.com>
2020-03-11 11:13:30 +0100

    Add full-width button in the footer!

--- web/src/pages/CoverpagePage/CoverpagePage.js
+++ web/src/pages/CoverpagePage/CoverpagePage.js
@@ -2,12 +2,34 @@ const CoverpagePage = () => {
   return (
   // layout
     <div className="mx-auto sm:px-6 lg:px-8 bg-indigo-600 h-screen">
+
       <div className="flex flex-col justify-center items-center pt-8">
-      <h3 className="text-lg leading-6 font-medium text-white">
-        Överkropp
-      </h3>
-      <p className="mt-4 text-white font-semibold text-3xl text-center">Hur stor yta täcker det?</p>
+        <h3 className="text-lg leading-6 font-medium text-white">
+          Överkropp
+        </h3>
+        <p className="mt-4 text-white font-semibold text-3xl text-center">Hur stor yta täcker det?</p>
       </div>
+
+
+
+
+      <div className="fixed inset-x-0 bottom-0">
+        <div className="bg-indigo-600">
+          <div className="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between flex-wrap">
+              <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0">
+                <div className="rounded-md shadow-sm pb-8">
+                  <a href="#" className="flex items-center justify-center px-4 py-4 border border-transparent text-sm leading-5 font-medium rounded-md bg-orange-300 hover:text-indigo-500 focus:outline-none focus:shadow-outline transition ease-in-out duration-150 rounded-full text-white font-medium text-xl">
+                    Vidare
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
